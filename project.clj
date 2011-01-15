6721e953ef88a606c4ff18bac4ec4882168cfab5
masaedw <masa.edw@gmail.com>
2011-01-15 12:21:46 +0900

    added dependencies

--- project.clj
+++ project.clj
@@ -1,4 +1,7 @@
-(defproject twimemo "1.0.0-SNAPSHOT"
-  :description "FIXME: write"
+(defproject twimemo "0.1.0-SNAPSHOT"
+  :description "memo sharing application over twitter"
   :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]])
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [compojure "0.5.3"]
+                 [ring/ring-jetty-adapter "0.3.1"]]
+  :dev-dependencies [[swank-clojure "1.2.1"]])
