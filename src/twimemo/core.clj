114dab942199fbd524f6e7a5143600fac24b9d39
masaedw <masa.edw@gmail.com>
2011-01-15 19:25:57 +0900

    implemented skelton of show and index memos

--- src/twimemo/core.clj
+++ src/twimemo/core.clj
@@ -3,14 +3,16 @@
         ring.util.response)
   (:require [appengine-magic.core :as ae]
             [appengine-magic.services.datastore :as ds])
-  (:import java.util.Date))
+  (:import java.util.Date
+           java.util.UUID))
 
-(ds/defentity Memo [^:key content users])
-(ds/defentity Author [^:key name birthday])
+(ds/defentity Memo [^:key uuid content users])
 
-(defroutes twimemo-app-handler
-  (GET "/memos/create" []
-       "
+(defn uuid []
+  (str (UUID/randomUUID)))
+
+(defn create-memo [req]
+  "
 <html>
   <head></head>
   <body>
@@ -20,29 +22,51 @@
     </form>
   </body>
 </html>")
-  (POST "/memos/create" request
-        (let [content (get-in request [:params "memo"])
-              x (Author. content nil)]
-          (ds/save! x)
-          (redirect "/memos")))
-  (GET "/memos" []
-       (str
-"
+
+(defn create-memo! [req]
+  (let [content (get-in req [:params "memo"])
+        x (Memo. (uuid) content nil)]
+    (ds/save! x)
+    (redirect "/memos")))
+
+(defn link-to [url name]
+  (format "<a href='%s'>%s</a>" url name))
+
+(defn link-to-memo [memo]
+  (link-to (format "/memos/%s" (:id memo))
+           (format "memo-%s" (:id memo))))
+
+(defn join [s coll]
+  (pr-str (interpose s coll)))
+
+(defn index-memo [req]
+  (str
+   "
 <html>
   <head></head>
   <body>
-    <h1>memo list</h1>"
-(vec (map (fn [x] (.name x)) (ds/query :kind twimemo.core.Author)))
-"
+    <h1>メモの一覧</h1>"
+   (join "<br>" (map link-to-memo (ds/query :kind Memo)))
+   "
   </body>
 </html>"))
-  (GET "/" [] "<h1>Hello World</h1>")
-  (GET "/hello/:name" [name]
-       {:status 200
-        :headers {"Content-Type" "text/plain"}
-        :body (format "Hello, %s!" name)})
+
+(defn show-memo [req]
+  (let [id (get-in req [:params "id"])
+        memo (ds/query :kind Memo :filter (= :id id))]
+    (pr-str memo)
+    ))
+
+(defroutes twimemo-app-handler
+  (GET "/memos/create" req (create-memo req))
+  (POST "/memos/create" req (create-memo! req))
+  (GET "/memos" req (index-memo req))
+  (GET "/memos/:id" req (show-memo req))
+  (GET "/" req (index-memo req))
+  (GET "/hoge" req
+       "hoge")
   (ANY "*" _
-       {:status 200
+       {:status 404
         :headers {"Content-Type" "text/plain"}
         :body "not found"}))
 
