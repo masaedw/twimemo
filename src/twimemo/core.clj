3cf5f57ad2b110d23b70717361346c630c48465d
masaedw <masa.edw@gmail.com>
2011-01-15 18:47:46 +0900

    request sample

--- src/twimemo/core.clj
+++ src/twimemo/core.clj
@@ -1,8 +1,41 @@
 (ns twimemo.core
-  (:use compojure.core)
-  (:require [appengine-magic.core :as ae]))
+  (:use compojure.core
+        ring.util.response)
+  (:require [appengine-magic.core :as ae]
+            [appengine-magic.services.datastore :as ds])
+  (:import java.util.Date))
+
+(ds/defentity Memo [^:key content users])
+(ds/defentity Author [^:key name birthday])
 
 (defroutes twimemo-app-handler
+  (GET "/memos/create" []
+       "
+<html>
+  <head></head>
+  <body>
+    <form method='post'>
+      <input type='text' name='memo' />
+      <input type='submit' />
+    </form>
+  </body>
+</html>")
+  (POST "/memos/create" request
+        (let [content (get-in request [:params "memo"])
+              x (Author. content nil)]
+          (ds/save! x)
+          (redirect "/memos")))
+  (GET "/memos" []
+       (str
+"
+<html>
+  <head></head>
+  <body>
+    <h1>memo list</h1>"
+(vec (map (fn [x] (.name x)) (ds/query :kind twimemo.core.Author)))
+"
+  </body>
+</html>"))
   (GET "/" [] "<h1>Hello World</h1>")
   (GET "/hello/:name" [name]
        {:status 200
@@ -14,3 +47,8 @@
         :body "not found"}))
 
 (ae/def-appengine-app twimemo-app #'twimemo-app-handler)
+
+;; (use :reload 'twimemo.core)
+;; (require '[appengine-magic.core :as ae])
+;; (ae/start twimemo-app :port 8080)
+;; (require '[appengine-magic.services.datastore :as ds])
