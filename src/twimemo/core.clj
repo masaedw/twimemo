7a5e422045f796a0e2f61206e6ec674f262ea630
masaedw <masa.edw@gmail.com>
2011-01-15 15:04:06 +0900

    sample project

--- src/twimemo/core.clj
+++ src/twimemo/core.clj
@@ -1,1 +1,16 @@
-(ns twimemo.core)
+(ns twimemo.core
+  (:use compojure.core)
+  (:require [appengine-magic.core :as ae]))
+
+(defroutes twimemo-app-handler
+  (GET "/" [] "<h1>Hello World</h1>")
+  (GET "/hello/:name" [name]
+       {:status 200
+        :headers {"Content-Type" "text/plain"}
+        :body (format "Hello, %s!" name)})
+  (ANY "*" _
+       {:status 200
+        :headers {"Content-Type" "text/plain"}
+        :body "not found"}))
+
+(ae/def-appengine-app twimemo-app #'twimemo-app-handler)
